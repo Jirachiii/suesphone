dbed2d702d1c2932492bce88a6d4452291322371
Jirachiii <305648262@qq.com>
2015-11-19 18:56:29 +0800

    V1.01
    admin 添加 “显示全部”

--- admin.php
+++ admin.php
@@ -102,7 +102,9 @@
 	?>
 		</select>
 		<input class="btn btn-primary" type="submit" value="查询">
-		<a href="add.php" class="btn btn-primary pull-right">添加</a>	
+		<a href="add.php" class="btn btn-primary pull-right">添加</a>
+		<span class="pull-right">&nbsp;</span>
+		<input type="button" value="显示全部" onclick="window.location.href='admin_all.php'" class="btn btn-primary pull-right " >
 	</form>
 
 	</div>
